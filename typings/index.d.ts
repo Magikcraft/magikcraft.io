3fe568b2f432096f945309da8c6a37d6985e971c
Joshua Wulf <sitapati@worldsankirtan.net>
2017-10-23 01:52:03 +1000

    Add EventEmitter typing

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -194,7 +194,7 @@ declare namespace Java {
 
 interface magik {
 
-    Events: EventEmitter;
+    Events: EventEmitter.EventEmitter;
     /**
      *
      * A HashMap that survives engine reload, but not quitting and rejoining the server.
@@ -750,3 +750,49 @@ import * as _mock from '../lib/mock';
 declare const mock: typeof _mock;
 
 **/
+
+declare namespace EventEmitter {
+    type EventKey = string | RegExp;
+
+    interface EventMap {
+        [event: string]: EventKey;
+    }
+
+    type Events = EventKey | EventMap;
+
+    interface Listener {
+        listener: Function;
+        once: boolean;
+    }
+
+    interface ListenerMap {
+        [event: string]: Listener[];
+    }
+
+    type Listeners = Listener[] | ListenerMap;
+
+    export class EventEmitter<E = EventKey> {
+        static noConflict(): typeof EventEmitter;
+
+        getListeners(event: E): Listeners;
+        flattenListeners(listeners: Listener[]): void;
+        getListenersAsObject(event: E): ListenerMap;
+        addListener(event: E, listener: Listener | Function): this;
+        on(event: E, listener: Listener | Function): this;
+        addOnceListener(event: E, listener: Function): this;
+        once(event: E, listener: Function): this;
+        defineEvent(event: E): this;
+        defineEvents(events: E[]): this;
+        removeListener(event: E, listener: Function): this;
+        off(event: E, listener: Function): this;
+        addListeners(event: Events, listeners: Function[]): this;
+        removeListeners(event: Events, listeners: Function[]): this;
+        manipulateListeners(remove: boolean, event: Events, listeners: Function[]): this;
+        removeEvent(event?: E): this;
+        removeAllListeners(event?: E): this;
+        emitEvent(event: E, args?: any[]): this;
+        trigger(event: E, args?: any[]): this;
+        emit(event: E, ...args: any[]): this;
+        setOnceReturnValue(value: any): this;
+    }
+}
